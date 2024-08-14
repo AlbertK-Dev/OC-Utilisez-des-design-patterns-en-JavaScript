ce441bbf6df7e604e0947788c0ed069138c8529e
Albert-K <albertkwebproglearning@gmail.com>
2024-08-14 16:34:49 +0100

    finish exercice

--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,3 +1,33 @@
 class Movie {
 
+    constructor(data){
+        this._title = data.title.fr || data.title.en;
+        this._duration = `${parseInt(Number(data.duration)/60)}h${Number(data.duration)%60}` ;
+        this._picture = data.picture;
+        this._synopsis = data.synopsis;
+        this._released_in = data.released_in;
+    }
+
+    get title(){
+        return this._title;
+    }
+
+    get duration(){
+        return this._duration;
+    }
+
+    get synopsis(){
+        return this._synopsis;
+    }
+    get released_in(){
+        return this._released_in;
+    }
+
+    get picture(){
+        return `/assets/${this._picture}`
+    }
+
+    get tumbnails(){
+        return `/assets/tumbnails/${this._picture}`
+    }
 }

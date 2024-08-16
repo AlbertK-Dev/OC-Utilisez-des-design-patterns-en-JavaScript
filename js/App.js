3736ce1c59b976d2ebdf86b86d2e76cb9fd78f51
Albert-K <albertkwebproglearning@gmail.com>
2024-08-16 10:23:49 +0100

    implement object no shoxing in dom

--- js/App.js
+++ js/App.js
@@ -4,12 +4,36 @@ const fetchData = () => fetch('/data/data.json')
 
 
 class Movie {
+    constructor(data){
+        this._id = data.id;
+        this._title = data.title;
+        this._img = data.img;
+        this._rating = data.rating;
+
+    }
+
+    get id(){
+        return this._id;
+    }
+    get title(){
+        return this._title;
+    }
+    get img(){
+        return this._img;
+    }
+    get rating(){
+        return this._rating;
+    }
+  
 }
 
 
 class App {
     async init() {
         const data = await fetchData()
+        console.log(data)
+     const   movies = data.movies.map(m=>new Movie(m));
+     console.log(movies)
     }
 }
 
